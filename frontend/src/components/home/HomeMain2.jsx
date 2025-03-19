674c124ce9eb9f3907f318c739359b64a6e1ad0f
윤동욱 <appdw1257@naver.com>
2025-03-19 13:38:52 +0900

    [Style] homemain style finish #frontend

--- frontend/src/components/home/HomeMain2.jsx
+++ frontend/src/components/home/HomeMain2.jsx
@@ -2,11 +2,11 @@
 import React from 'react';
 import '../../styles/home/Home2.css';
 import MainPhone from '../../assets/image/homemain2/Main2_phone.png';
-import MainGreenCircle1 from '../../assets/image/homemain2/Main2_green_circle1.png';
-import MainGreenCircle2 from '../../assets/image/homemain2/Main2_green_circle2.png';
-import MainYellowCircle1 from '../../assets/image/homemain2/Main2_yellow_circle1.png';
-import MainDot1 from '../../assets/image/homemain2/Main2_dot1.png';
-import MainDot2 from '../../assets/image/homemain2/Main2_dot2.png';
+import MainGreenCircle1 from '../../assets/image/homemain2/Main2_green_circle1.svg';
+import MainGreenCircle2 from '../../assets/image/homemain2/Main2_green_circle2.svg';
+import MainYellowCircle1 from '../../assets/image/homemain2/Main2_yellow_circle1.svg';
+import MainDot1 from '../../assets/image/homemain2/Main2_dot1.svg';
+import MainDot2 from '../../assets/image/homemain2/Main2_dot2.svg';
 
 const HomeMain2 = () => {
   return (
@@ -105,7 +105,7 @@ const HomeMain2 = () => {
           height: '24px',
           pointerEvents: 'none',
           objectFit: 'contain',
-          zIndex: '2',
+          zIndex: '0',
           opacity: '0',
           animationDelay: '1s',
         }}
@@ -123,22 +123,28 @@ const HomeMain2 = () => {
           height: '25px',
           pointerEvents: 'none',
           objectFit: 'contain',
-          zIndex: '2',
+          zIndex: '0',
           opacity: '0',
           animationDelay: '1.2s',
         }}
       />
 
-      <div className="feature-image slide-from-left">
+      <div
+        className="feature-image slide-from-left"
+        style={{ position: 'relative', zIndex: '2' }}
+      >
         <img
           src={MainPhone}
           alt="음성 번역 앱"
           className="phone-img"
-          style={{ pointerEvents: 'none', position: 'relative', zIndex: '1' }}
+          style={{ pointerEvents: 'none', position: 'relative', zIndex: '2' }}
         />
       </div>
 
-      <div className="feature-content right-align slide-from-right">
+      <div
+        className="feature-content right-align slide-from-right"
+        style={{ position: 'relative', zIndex: '2' }}
+      >
         <h3 className="feature-title">
           <span className="normal-text">원활한 소통을 위한</span>
           <span className="highlight">음성 번역</span>
