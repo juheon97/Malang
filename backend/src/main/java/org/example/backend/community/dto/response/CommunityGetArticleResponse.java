7145f62040750c48962d1b2e19789d0b4f280907
Juheon Song <juheonit2024@gmail.com>
2025-03-25 05:21:46 +0900

    feat: 글 상세 조회 S12P21D110-226

---
+++ backend/src/main/java/org/example/backend/community/dto/response/CommunityGetArticleResponse.java
@@ -0,0 +1,21 @@
+package org.example.backend.community.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommunityGetArticleResponse {
+    private Integer article_id;
+    private String category;
+    private String title;
+    private LocalDateTime created_at;
+    private Integer likes;
+    private Boolean is_liked;
+}
