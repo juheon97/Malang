7d48e93a00b35a8695c6f51c3b302e8ec467117b
윤동욱 <appdw1257@naver.com>
2025-03-26 17:32:50 +0900

    [fix] login authentication fix #frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,9 @@ import { AuthProvider } from './contexts/AuthContext';
 import Layout from './components/layout/Layout';
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
-import Community from './pages/community/community';
+import CommunityList from './components/community/CommunityList';
+import CommunityWrite from './components/community/CommunityWrite';
+import CommunityDetail from './components/community/CommunityDetail';
 import Counsel from './pages/counsel/Counsel';
 import VoiceChannel from './pages/voicechannel/VoiceChannel';
 import Signup from './pages/signup/Signup';
@@ -15,7 +17,6 @@ import CounselChannelRoom from './pages/counselchannel/CounselChannelRoom';
 import CounselChannelVideo from './pages/counselchannel/CounselChannelVideo';
 import VoiceChange from './pages/VoiceChange';
 import ProtectedRoute from './components/common/ProtectedRoute';
-import CommunityWrite from './components/community/CommunityWrite';
 
 function App() {
   return (
@@ -27,11 +28,8 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/Signup" element={<Signup />} />
-            <Route path="/mypage" element={<Mypage />} />
-            <Route path="/community/*" element={<Community />} />
-            <Route path="/counsel-channel" element={<Counsel />} />
-            <Route path="/voice-channel" element={<VoiceChannel />} />
-            <Route path="/voice-channel-room" element={<VoiceChannelRoom />} />
+            <Route path="/community" element={<CommunityList />} />
+            <Route path="/community/:id" element={<CommunityDetail />} />
 
             {/* 보호된 라우트 - 인증 필요 */}
             <Route
