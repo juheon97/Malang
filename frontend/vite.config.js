74117ffc77ec8ab9facca20bfa416589a1e6e717
윤동욱 <appdw1257@naver.com>
2025-03-26 13:21:24 +0900

    [feature] server setting #frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,4 +9,10 @@ export default defineConfig({
     host: true,
     allowedHosts: ['j12d110.p.ssafy.io', 'all'],
   },
+  server: {
+    allowedHosts: [
+      'e27c-14-46-141-158.ngrok-free.app', // 이건 ngrok 서버, 안되면 이거 주석처리하고 EC2 연결 바람
+      'backend.takustory.site/api', //백엔드 팀장 컴퓨터 서버 켜져있어야 작동, 안되면 주석처리
+    ],
+  },
 });
