c2895c86d89e4fc65b59c9503046cad22ad64698
윤동욱 <appdw1257@naver.com>
2025-03-27 14:17:33 +0900

    [fix] websocket connection server url change #frontend

--- frontend/src/api/authApi.js
+++ frontend/src/api/authApi.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
-const isDevelopment = import.meta.env.MODE === 'development';
-const API_URL = isDevelopment
-  ? 'http://localhost:8080/api'
-  : 'https://J12D110.p.ssafy.io/api';
+const API_URL =
+  import.meta.env.VITE_API_URL || 'https://J12D110.p.ssafy.io/api';
 
 // 환경 변수에서 모의 API 사용 여부 가져옴
 const USE_MOCK_API = import.meta.env.VITE_USE_MOCK_API === 'true';
