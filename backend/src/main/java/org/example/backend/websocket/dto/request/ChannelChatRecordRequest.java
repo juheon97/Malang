c3745141708cf7cb4b4fc4858b72366b12d16611
Juheon Song <juheonit2024@gmail.com>
2025-04-09 14:30:32 +0900

    fix : log message 수정하였습니다.

--- backend/src/main/java/org/example/backend/websocket/dto/request/ChannelChatRecordRequest.java
+++ backend/src/main/java/org/example/backend/websocket/dto/request/ChannelChatRecordRequest.java
@@ -2,6 +2,6 @@ package org.example.backend.websocket.dto.request;
 
 import java.time.LocalDateTime;
 
-public record ChannelChatRecordRequest(String event, String content, Long user, String nickname, LocalDateTime currentTime,  String role){
+public record ChannelChatRecordRequest(String event, String content,Long channel, Long user, String nickname, LocalDateTime currentTime,  String role){
 
 }
